849b8aef092e8d654a5113ccb3827e9fa8e1a044
CatherineDevapriya <93270875+CatherineDevapriya@users.noreply.github.com>
2021-11-02 12:47:21 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,173 @@
+setInterval(one,3000);
+        setInterval(two,6000);
+        setInterval(three,9000);
+        function one()
+        {
+            document.getElementById("h1").innerHTML="Business is Now Digital";
+            document.getElementById("img1").src="1.png";
+        
+        }
+        function two()
+        {
+            document.getElementById("h1").innerHTML="Crafted for Business";
+            document.getElementById("img1").src="2.png";
+        
+        }
+        function three()
+        {
+            document.getElementById("h1").innerHTML="Based on Bootstrap 4";
+            document.getElementById("img1").src="3.png";
+        
+        }
+        function four()
+        {
+            document.getElementById("id2").style.display="inline"; 
+            document.getElementById("id3").style.display="inline";
+            document.getElementById("id4").style.display="inline";
+            document.getElementById("id5").style.display="inline";
+            document.getElementById("id6").style.display="inline";
+            document.getElementById("id7").style.display="inline";
+            document.getElementById("id8").style.display="inline";
+            document.getElementById("id9").style.display="inline";
+            document.getElementById("id10").style.display="inline";
+        
+        }
+            
+
+        
+        function five()
+        { 
+            document.getElementById("id2").style.display="inline";
+            document.getElementById("id3").style.display="none";
+            document.getElementById("id4").style.display="inline";
+            document.getElementById("id5").style.display="none";
+            document.getElementById("id6").style.display="none";
+            document.getElementById("id7").style.display="none";
+            document.getElementById("id8").style.display="none";
+            document.getElementById("id9").style.display="inline";
+            document.getElementById("id10").style.display="none";
+            
+        }
+        function six()
+        {
+            document.getElementById("id2").style.display="none"; 
+            document.getElementById("id3").style.display="inline";
+            document.getElementById("id4").style.display="inline";
+            document.getElementById("id5").style.display="none";
+            document.getElementById("id6").style.display="inline";
+            document.getElementById("id7").style.display="none";
+            document.getElementById("id8").style.display="none";
+            document.getElementById("id9").style.display="none";
+            document.getElementById("id10").style.display="inline";
+            
+        }
+        function seven()
+        {
+            document.getElementById("id2").style.display="inline"; 
+            document.getElementById("id3").style.display="none";
+            document.getElementById("id4").style.display="none";
+            document.getElementById("id5").style.display="inline";
+            document.getElementById("id6").style.display="none";
+            document.getElementById("id7").style.display="inline";
+            document.getElementById("id8").style.display="none";
+            document.getElementById("id9").style.display="inline";
+            document.getElementById("id10").style.display="none";
+            
+        }
+        function eight()
+        {
+            document.getElementById("id2").style.display="none"; 
+            document.getElementById("id3").style.display="inline";
+            document.getElementById("id4").style.display="none";
+            document.getElementById("id5").style.display="inline";
+            document.getElementById("id6").style.display="none";
+            document.getElementById("id7").style.display="none";
+            document.getElementById("id8").style.display="inline";
+            document.getElementById("id9").style.display="none";
+            document.getElementById("id10").style.display="none";
+        }
+        
+        function visibility1()
+        {
+            document.getElementById("a").style.display="block";
+            document.getElementById("1").innerHTML="&#x25BC;";
+        }
+        function hide1(){
+            document.getElementById("a").style.display="none";
+            document.getElementById("1").innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;&gt;";
+        }
+        function visibility2()
+        {
+            document.getElementById("b").style.display="block";
+            document.getElementById("2").innerHTML="&#x25BC;";
+        }
+        function hide2(){
+            document.getElementById("b").style.display="none";
+            document.getElementById("2").innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;&gt;"
+        }
+        function visibility3()
+        {
+            document.getElementById("c").style.display="block";
+            document.getElementById("3").innerHTML="&#x25BC;";
+            
+        }
+        function hide3(){
+            document.getElementById("c").style.display="none";
+            document.getElementById("3").innerHTML="&nbsp;&nbsp;&gt;"
+        }
+        function visibility4()
+        {
+            document.getElementById("d").style.display="block";
+            document.getElementById("4").innerHTML="&#x25BC;";
+        }
+        function hide4(){
+            document.getElementById("d").style.display="none";
+            document.getElementById("4").innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;&gt;"
+        }
+        function visibility5()
+        {
+            document.getElementById("e").style.display="block";
+            document.getElementById("5").innerHTML="&#x25BC;";
+        }
+        function hide5(){
+            document.getElementById("e").style.display="none";
+            document.getElementById("5").innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;&gt;"
+        }
+        setInterval(author,3000);
+        function author()
+        {
+            document.getElementById("img2").src="author-2.jpg";
+        }
+        setInterval(author1,6000);
+        function author1()
+        {
+            document.getElementById("img2").src="author-3.jpg";
+        }
+        setInterval(author2,9000);
+        function author2()
+        {
+            document.getElementById("img2").src="author-1.jpg";
+        }
+        function icon1(){
+            document.getElementById("t1").style.display="block";
+        }
+        function iconremove1(){
+            document.getElementById("t1").style.display="none";
+        }
+        function icon2(){
+            document.getElementById("t2").style.display="block";
+        }
+        function iconremove2(){
+            document.getElementById("t2").style.display="none";
+        }
+        function icon3(){
+            document.getElementById("t3").style.display="block";
+        }
+        function iconremove3(){
+            document.getElementById("t3").style.display="none";
+        }
+        function error(){
+            
+             document.getElementById("err").style.display="block";
+
+        }
